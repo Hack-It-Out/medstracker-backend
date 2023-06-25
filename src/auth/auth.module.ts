c2af14dbb9631cd95559788764112f8cd6f946e9
paulndungu <p.ndungu.kara@gmail.com>
2023-06-26 02:39:26 +0300

    Added sorting user by id, password hiding and finished up on the project

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from 'prisma/prisma.service';
 import {JwtModule} from "@nestjs/jwt";
+import {PassportModule} from "@nestjs/passport"
 
 @Module({
-  imports:[JwtModule],
+  imports:[JwtModule,PassportModule],
   controllers: [AuthController],
   providers: [AuthService]
 })
